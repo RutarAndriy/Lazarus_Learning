ce026a0b8cc9d7a5a48a8cfa604b420223f80276
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-13 19:13:17 +0200

    Added 02_Project_Anatomy/02

---
+++ 02_Project_Anatomy/02/main.pas
@@ -0,0 +1,40 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TfMain }
+
+  TfMain = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  fMain: TfMain;
+
+implementation
+
+{$R *.lfm}
+
+{ TfMain }
+
+procedure TfMain.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Привіт, ' + Edit1.Text + '!');
+end;
+
+end.
+
